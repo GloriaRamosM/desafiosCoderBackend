4b4c722a67b1d01a5e01e9056cf72cdf04973b56
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-03-01 00:05:08 -0300

    desafio comenzado ( muestro con handlebars, los productos del json

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import productsRouter from "./src/routes/productsRouter.js";
 import cartsRouter from "./src/routes/cartsRouter.js";
+import viewsRouter from "./src/routes/views.router.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,13 +15,26 @@ const app = express();
 const port = 8080;
 
 //Middlewares
+app.set("views", __dirname + "/src/views");
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
+app.engine("handlebars", handlebars.engine());
+
+app.use(viewsRouter);
 
 ////Routes
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-app.listen(port, () => console.log("servidor corriendo en el puerto " + port));
+const server = app.listen(port, () =>
+  console.log("servidor corriendo en el puerto " + port)
+);
+
+const io = new Server(server); // instanciando socket.io
+
+// io.on("connection", (socket) => {
+//   console.log("Connected!");
+// });
---
+++ src/routes/views.router.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const router = Router();
+const productosFilePath = join(__dirname, "../data/productos.json");
+
+router.get("/", async (req, res) => {
+  try {
+    const productosData = await fs.readFile(productosFilePath, "utf-8");
+    const productos = JSON.parse(productosData);
+    res.render("home", { productos: productos });
+  } catch (error) {
+    console.error("Error al leer el archivo JSON:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+
+export default router;
